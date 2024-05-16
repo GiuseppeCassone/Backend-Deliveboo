757c444dbae06a5638aa5ce7adc6e571801ce9e3
simoneditaranto <simodita99@gmail.com>
2024-05-16 19:40:32 +0200

    feat: add soft delete in dishes

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dish extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'description', 'ingredients' ,'is_visible', 'restaurant_id'];
+
+    use SoftDeletes;
 
     // relazione di Dishes con restaurant
     public function restaurant(){
