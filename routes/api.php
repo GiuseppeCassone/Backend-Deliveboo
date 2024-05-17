5f7f2e9fcbd95e2fa5397a7bdf78cd1a99cc934b
albertoferri <falberto99@gmail.com>
2024-05-17 11:55:03 +0200

    aggiunta rotta e controller dishes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\DishController;
 use App\Http\Controllers\Api\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/restaurant', [RestaurantController::class, 'index']);
+
+
+Route::get('/dishes', [DishController::class, 'index']);
