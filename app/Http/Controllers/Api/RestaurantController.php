4b26ab4df5f050266176fbdd1a76431c766184a1
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-05-19 16:08:23 +0200

    restaurants filter api completed

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -8,11 +8,24 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         
         $query = Restaurant::with('typologies');
 
         // $types = request('typologies');
+
+        if($request->has('typologies')) {
+
+            $types = $request->input('typologies');
+
+            $typesArray = explode(',', $types);
+            if($typesArray){
+                $query->whereHas('typologies', function ($query) use ($typesArray){
+                    $query->whereIn('type', $typesArray);
+                });
+                
+            }
+        }
 
         // $typesArray = explode(',', $types);
 
@@ -22,11 +35,11 @@ class RestaurantController extends Controller
         //     });
         // }
 
-        if (request('type')) {
-            $query->whereHas('typologies', function ($query) {
-                $query->where('type', request('type'));
-            });
-        }
+        // if (request('type')) {
+        //     $query->whereHas('typologies', function ($query) {
+        //         $query->where('type', request('type'));
+        //     });
+        // }
         $restaurants = $query->get();
         // con get() li prendo tutti, nel caso dovessi mettere una paginazione al posto del get() ci andrà paginate();
 
